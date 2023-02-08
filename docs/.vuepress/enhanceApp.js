7435392dbbfef5f897146ff8f7bdb9a1b674d4e5
qinghuanI <1062217965@qq.com>
2023-02-08 17:46:12 +0800

    fix issues by qodana

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 import Element from "element-ui"
 import "element-ui/lib/theme-chalk/index.css"
 
-export default ({ Vue, options, router }) => {
-    Vue.use(Element)
+export default ({ Vue }) => {
+  Vue.use(Element)
 }
